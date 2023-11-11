01e090528cd86bdccb5bcc510e9c5786929f4e7b
Siarhei Paulavets <s.paulavets@gmail.com>
2023-11-11 18:38:40 +0300

    Добавлен интерфейс для работы с контекстом БД

---
+++ ApplicationCore/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationCore.Interfaces;
+
+/// <summary>
+/// UOW interface for database access
+/// </summary>
+public interface IApplicationDbContext
+{
+    /// <summary>
+    /// Meetups table
+    /// </summary>
+    DbSet<Meetup> Meetups { get; }
+
+    /// <summary>
+    /// Commit transaction in database
+    /// </summary>
+    /// <returns>Asynchronous save operation resulting in number of affected rows</returns>
+    Task<int> SaveChangesAsync();
+}
