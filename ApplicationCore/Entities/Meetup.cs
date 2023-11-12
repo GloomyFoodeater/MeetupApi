67dd3cd5b62af802feffa066c8c38d6de0d0e6c3
Siarhei Paulavets <s.paulavets@gmail.com>
2023-11-12 23:59:22 +0300

    Fix service to use repository pattern and dtos, minor entity fix

--- ApplicationCore/Entities/Meetup.cs
+++ ApplicationCore/Entities/Meetup.cs
@@ -2,23 +2,21 @@
 
 public class Meetup
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-    public string? Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 
-    public string? Agenda { get; set; }
+    public string Agenda { get; set; } = string.Empty;
 
-    public string Organizer { get; set; }
+    public string Organizer { get; set; } = string.Empty;
 
-    public string Speaker { get; set; }
+    public string Speaker { get; set; } = string.Empty;
 
     public DateTime StartDateTime { get; set; }
 
     public DateTime EndDateTime { get; set; }
 
-    public string Location { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string Location { get; set; } = string.Empty;
 }
--- ApplicationCore/Services/MeetupService.cs
+++ ApplicationCore/Services/MeetupService.cs
@@ -1,54 +1,77 @@
-﻿using ApplicationCore.Entities;
-using ApplicationCore.Interfaces;
-using Microsoft.EntityFrameworkCore;
+﻿using ApplicationCore.DTO.Meetup;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces.Repositories;
+using AutoMapper;
 
 namespace ApplicationCore.Services;
 
 /// <summary>
-/// Service for CRUD operations on meetups in the database
+/// Service for CRUD operations on meetups in the database.
+/// Wraps <see cref="IMeetupRepository"/> methods with data transfer and
+/// exception throwing.
 /// </summary>
 public class MeetupService
 {
-    private readonly IApplicationDbContext _context;
+    private readonly IMeetupRepository _repo;
+    private readonly IMapper _mapper;
+    
+    /// <summary>
+    /// Create service via repository and mapper
+    /// </summary>
+    /// <param name="repo">Repository to obtain data</param>
+    /// <param name="mapper">Mapper to map meetup dtos with meetups</param>
+    public MeetupService(IMeetupRepository repo, IMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
 
-    public MeetupService(IApplicationDbContext context) => _context = context;
+    private async Task<Meetup> GetOrThrow(int id)
+    {
+        return await _repo.GetAsync(id)
+            ?? throw new KeyNotFoundException($"Meetup with id='{id}' did not exist");
+    }
 
     /// <summary>
     /// Get list of all meetups
     /// </summary>
     /// <returns>Asynchronous get operation resulting in list of meetups</returns>
-    public async Task<List<Meetup>> GetAllAsync() => await _context.Meetups.ToListAsync();
+    public async Task<List<Meetup>> GetAllAsync() => await _repo.GetAllAsync();
 
     /// <summary>
     /// Get meetup by given id
     /// </summary>
     /// <param name="id">Id of meetup to get</param>
     /// <returns>Asynchronous get operation resulting in found meetup or null</returns>
-    public async Task<Meetup?> GetByIdAsync(int id) => await _context.Meetups.FindAsync(id);
+    public async Task<Meetup?> GetAsync(int id) => await _repo.GetAsync(id);
 
-    // TODO: Pass dto
     /// <summary>
     /// Add new meetup
     /// </summary>
-    /// <param name="data">Data to create meetup</param>
-    /// <returns>Asynchronous add operation resulting in id of added meetup</returns>
-    public async Task<int> AddAsync(Meetup data)
+    /// <param name="data">Data to add meetup</param>
+    /// <returns>Asynchronous add operation resulting in added meetup</returns>
+    public async Task<Meetup> AddAsync(MeetupAddDto data)
     {
-        _context.Meetups.Add(data);
-        await _context.SaveChangesAsync();
-        return data.Id;
+        var entity = _mapper.Map<MeetupAddDto, Meetup>(data);
+        _repo.Add(entity);
+        await _repo.Commit();
+        return entity;
     }
 
-    // TODO: Pass dto
     /// <summary>
     /// Update existing meetup
     /// </summary>
-    /// <returns>Asynchronous update operation</returns>
+    /// <param name="id">Id of meetup to update</param>
     /// <param name="data">Data to update meetup</param>
-    public async Task UpdateAsync(Meetup data)
+    /// <returns>Asynchronous update operation resulting in updated meetup</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if there was no entity with given id</exception>
+    public async Task<Meetup> UpdateAsync(int id, MeetupUpdateDto data)
     {
-        _context.Meetups.Update(data);
-        await _context.SaveChangesAsync();
+        var entity = await GetOrThrow(id);
+        _mapper.Map(source: data, destination: entity);
+        _repo.Update(entity);
+        await _repo.Commit();
+        return entity;
     }
 
     /// <summary>
@@ -59,8 +82,8 @@ public class MeetupService
     /// <exception cref="KeyNotFoundException">Thrown if there was no entity with given id</exception>
     public async Task RemoveAsync(int id)
     {
-        var meetup = await _context.Meetups.FindAsync(id)
-            ?? throw new KeyNotFoundException($"Meetup with id='{id}' was not found");
-        _context.Meetups.Remove(meetup);
+        var entity = await GetOrThrow(id);
+        _repo.Remove(entity);
+        await _repo.Commit();
     }
 }
