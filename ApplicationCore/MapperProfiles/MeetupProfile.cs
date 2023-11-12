90cfcdfa545b146f8a211b9bede56e7787224bfe
Siarhei Paulavets <s.paulavets@gmail.com>
2023-11-13 00:07:35 +0300

    Add mapper configuration

---
+++ ApplicationCore/MapperProfiles/MeetupProfile.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.DTO.Meetup;
+using ApplicationCore.Entities;
+using AutoMapper;
+
+namespace ApplicationCore.MapperProfiles;
+
+/// <summary>
+/// Profile to create maps for meetup dtos
+/// </summary>
+public class MeetupProfile : Profile
+{
+    public MeetupProfile()
+    {
+        CreateMap<MeetupAddDto, Meetup>();
+
+        // Ignore null members
+        CreateMap<MeetupUpdateDto, Meetup>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+    }
+}
