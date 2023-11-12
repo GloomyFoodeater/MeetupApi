337aac2e3a55c5620ce3f6c77b4bf4dc526b285e
Siarhei Paulavets <s.paulavets@gmail.com>
2023-11-12 23:59:55 +0300

    Implement repository

---
+++ Infrastructure/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+/// <summary>
+/// Implementation of EF core unit of work
+/// </summary>
+public class ApplicationContext : DbContext
+{
+    /// <summary>
+    /// Connect to database with given options and ensure its creation
+    /// </summary>
+    /// <param name="options">Options to configure db context</param>
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) => Database.EnsureCreated();
+
+    #region EF Core repositories
+
+    public DbSet<Meetup> Meetups => Set<Meetup>();
+
+    #endregion
+}
